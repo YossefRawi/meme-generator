d66b037998cbba8845bf279e49a2d62b6c502b19
Yossef Rawy <yossefrwy@gamil.com>
2023-02-13 10:57:25 +0200

    done

---
+++ react-meme-generator/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header.js"
+import Main from "./components/Main.js";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ react-meme-generator/src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+export default function Header(){
+
+    return(
+    <header> 
+        <img src="images/Troll Face.png" alt="troll-face" className="header--image"/>
+        <h1 className="header--title">Meme Generator</h1>
+        <h1 className="header--project">React Course - Project 3</h1>
+    </header>
+    )
+}
